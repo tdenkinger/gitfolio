863950dd47473672ef5f6a3606d7647956608a2b
Troy Denkinger <tdenkinger@gmail.com>
2014-06-15 11:02:58 -0400

    Hook up the basic repo view.

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,14 +1,11 @@
-require_relative "../../lib/adapters"
-
 class ReposController < ApplicationController
   def index
   end
 
   def show
     @user = params[:id]
-    @repos = ["hello"]
     @gh = Adapters::Github.new user: @user
-    # @repos = @gh.names
+    @repos = @gh.names
   end
 end
 
